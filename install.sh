01195602b6618a09bd575412e8d5112513549f8f
ddeity <ddeity@gmail.com>
2023-11-12 01:51:43 +0300

    install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+echo "Copying the executables"
+cp bin/killstreak /home/$SUDO_USER/.local/share/
+cp bin/parse_demo /home/$SUDO_USER/.local/share/
+echo "Creating service file"
+sudo touch /etc/systemd/user/killstreak.service
+sudo bash -c 'cat' << EOF > /etc/systemd/user/killstreak.service
+[Unit]
+Description=Killstreak service
+[Service]
+ExecStart=/home/$SUDO_USER/.local/share/killstreak
+Restart=on-failure
+[Install]
+WantedBy=default.target
+EOF
+echo "Reloading systemd daemon"
+systemctl --user -M $SUDO_USER@ daemon-reload
+echo "Enabling and starting the service"
+echo ---------------------------------------------
+echo | cat /etc/systemd/user/killstreak.service
+echo ---------------------------------------------
+systemctl --user -M $SUDO_USER@ enable --now killstreak.service
+systemctl --user -M $SUDO_USER@ status killstreak.service
